1d80e346aa891a0244f7652485a264c577e187cd
Your Name <you@example.com>
2024-02-04 19:40:50 +0200

    Adding getAllBooks method in BookService

--- src/main/java/book/com/library/service/BookService.java
+++ src/main/java/book/com/library/service/BookService.java
@@ -1,5 +1,6 @@
 package book.com.library.service;
 
+import book.com.library.dto.BookDTO;
 import book.com.library.model.Author;
 import book.com.library.model.Book;
 import book.com.library.repository.BookRepository;
@@ -9,14 +10,18 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class BookService {
     private BookRepository bookRepository;
 
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    public List<BookDTO> getAllBooks() {
+        List<Book> books = bookRepository.findAll();
+
+        return books.stream().map(book -> new BookDTO(book.getId(), book.getTitle(),book.getAuthorName()))
+                .collect(Collectors.toList());
     }
 
     public List<Book> findByTitleAndAuthor(String title, Author author) {
