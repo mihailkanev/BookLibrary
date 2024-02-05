d320046a2ead24ff7d98a6c61f0cd4d92d2934de
Your Name <you@example.com>
2024-02-06 01:39:18 +0200

    Implement getAllAuthors and getAuthorById methods in AuthorService

--- src/main/java/book/com/library/service/AuthorService.java
+++ src/main/java/book/com/library/service/AuthorService.java
@@ -1,8 +1,13 @@
 package book.com.library.service;
 
+import book.com.library.dto.AuthorDTO;
 import book.com.library.model.Author;
 import book.com.library.repository.AuthorRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthorService {
@@ -24,5 +29,23 @@ public class AuthorService {
         }
 
         return author;
+    }
+
+    public List<AuthorDTO> getAllAuthors() {
+        try {
+            List<Author> authors = authorRepository.findAll();
+            return authors.stream().map(author -> new AuthorDTO(author.getName())).collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving authors", e);
+        }
+    }
+
+    public AuthorDTO getAuthorById(Long id) {
+        try {
+            Optional<Author> optionalAuthor = authorRepository.findById(id);
+            return optionalAuthor.map(author -> new AuthorDTO(author.getName())).orElse(null);
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving author by ID", e);
+        }
     }
 }
