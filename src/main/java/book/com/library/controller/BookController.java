38ffa57ade408a42714498e707ac81696bcde7c6
Your Name <you@example.com>
2024-02-05 13:11:44 +0200

    Add endpoint in BookController to retrieve book by ID

--- src/main/java/book/com/library/controller/BookController.java
+++ src/main/java/book/com/library/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +20,21 @@ import java.util.List;
 public class BookController {
     @Autowired
     private BookService bookService;
+
     @GetMapping
-    public ResponseEntity<List<BookDTO>> getAllBooks(){
+    public ResponseEntity<List<BookDTO>> getAllBooks() {
         List<BookDTO> books = bookService.getAllBooks();
         return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
+        BookDTO bookDTO = bookService.getBookById(id);
+
+        if (bookDTO != null) {
+            return new ResponseEntity<>(bookDTO, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
