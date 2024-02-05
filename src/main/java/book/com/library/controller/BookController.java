7edc5d195a65fcb6d22b8745692916f7c6acc66a
Your Name <you@example.com>
2024-02-05 14:43:45 +0200

    Add endpoint in BookController to create new Book

--- src/main/java/book/com/library/controller/BookController.java
+++ src/main/java/book/com/library/controller/BookController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,4 +34,11 @@ public class BookController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @PostMapping
+    public ResponseEntity<BookDTO>createBook(@RequestBody BookDTO bookDTO){
+        BookDTO createdBook = bookService.createBook(bookDTO);
+        return new ResponseEntity<>(createdBook,HttpStatus.CREATED);
+    }
+
 }
