85105b25548eaac45dc21e2eca6509153d450dfc
Your Name <you@example.com>
2024-02-06 01:36:51 +0200

    Implement AuthorController for handling author-related endpoints

--- src/main/java/book/com/library/controller/AuthorController.java
+++ src/main/java/book/com/library/controller/AuthorController.java
@@ -1,5 +1,44 @@
 package book.com.library.controller;
 
-public class AuthorController{
+import book.com.library.dto.AuthorDTO;
+import book.com.library.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("api/authors")
+public class AuthorController {
+    @Autowired
+    private AuthorService authorService;
+
+    @GetMapping
+    public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
+        try {
+            List<AuthorDTO> authors = authorService.getAllAuthors();
+            return new ResponseEntity<>(authors, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorDTO> getAuthorById(@PathVariable Long id) {
+        try {
+            AuthorDTO authorDTO = authorService.getAuthorById(id);
+            if (authorDTO != null) {
+                return new ResponseEntity<>(authorDTO, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(authorDTO, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
