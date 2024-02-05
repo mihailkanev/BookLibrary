86edefa70a74e54c29d30103b2466261c11a3735
Your Name <you@example.com>
2024-02-06 01:35:09 +0200

    Add AuthorDTO for handling data in the application

---
+++ src/main/java/book/com/library/dto/AuthorDTO.java
@@ -0,0 +1,12 @@
+package book.com.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorDTO {
+    private String name;
+}
