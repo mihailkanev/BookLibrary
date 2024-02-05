e123ec629bdb8531a0882fe75bca2cc666842788
Your Name <you@example.com>
2024-02-05 14:45:38 +0200

    Add save method in BookRepository

--- src/main/java/book/com/library/repository/BookRepository.java
+++ src/main/java/book/com/library/repository/BookRepository.java
@@ -1,5 +1,6 @@
 package book.com.library.repository;
 
+import book.com.library.dto.BookDTO;
 import book.com.library.model.Author;
 import book.com.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +13,5 @@ import java.util.Optional;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByTitleAndAuthor(String title, Author author);
+    Book save(BookDTO bookDTO);
 }
