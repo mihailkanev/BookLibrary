03c199057d9ed51878960ebbec9d22bb75d134b7
Your Name <you@example.com>
2024-02-06 14:33:49 +0200

    Add endpoint in AuthorController to search authors by first letter

--- src/main/java/book/com/library/controller/AuthorController.java
+++ src/main/java/book/com/library/controller/AuthorController.java
@@ -5,10 +5,7 @@ import book.com.library.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,6 +33,20 @@ public class AuthorController {
                 return new ResponseEntity<>(authorDTO, HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(authorDTO, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/search-by-letter")
+    public ResponseEntity<List<AuthorDTO>> searchAuthorByFirstLetter(@RequestParam char letter) {
+        try {
+            List<AuthorDTO> authors = authorService.searchAuthorByFirstLetter(letter);
+            if (authors.isEmpty()) {
+                return new ResponseEntity<>(authors, HttpStatus.NOT_FOUND);
+            } else {
+                return new ResponseEntity<>(authors, HttpStatus.OK);
             }
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
