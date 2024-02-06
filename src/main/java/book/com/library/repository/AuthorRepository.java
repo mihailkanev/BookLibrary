457541e508cdc51f71e8b8e4d1a95ebb4e6592fd
Your Name <you@example.com>
2024-02-06 14:38:16 +0200

    Add repository  method in AuthorRepository to find author by name starting with a specified letter

--- src/main/java/book/com/library/repository/AuthorRepository.java
+++ src/main/java/book/com/library/repository/AuthorRepository.java
@@ -1,11 +1,14 @@
 package book.com.library.repository;
 
 import book.com.library.model.Author;
-import book.com.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author,Long > {
     Author findByName(String authorName);
+
+    List<Author> findByNameStartingWithIgnoreCase(char letter);
 }
