569f778d9ab4e3b5846b30d96b042152343340fb
Your Name <you@example.com>
2024-02-07 13:24:34 +0200

    Add repository method in BookRepository for pagination

--- src/main/java/book/com/library/repository/BookRepository.java
+++ src/main/java/book/com/library/repository/BookRepository.java
@@ -3,6 +3,8 @@ package book.com.library.repository;
 import book.com.library.dto.BookDTO;
 import book.com.library.model.Author;
 import book.com.library.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,5 +18,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findBookByTitleContainingIgnoreCase(String title);
     Book save(BookDTO bookDTO);
+    Page<Book> findAll(Pageable pageable);
+
 
 }
