8e1d8932b22330097470ab77d8d6a2d1804ba78f
Your Name <you@example.com>
2024-02-07 14:41:35 +0200

    Add reviewDTO for representing review data in application

---
+++ src/main/java/book/com/library/dto/ReviewDTO.java
@@ -0,0 +1,15 @@
+package book.com.library.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReviewDTO {
+    private Long id;
+
+    @NotBlank(message = "Review cannot be blank")
+    private Double rating;}
