a183936b15bb9418cd90839a7f2a34d434cf57ed
Your Name <you@example.com>
2024-02-07 14:44:17 +0200

    Implement method in ReviewService to get all reviews

--- src/main/java/book/com/library/service/ReviewService.java
+++ src/main/java/book/com/library/service/ReviewService.java
@@ -1,4 +1,18 @@
 package book.com.library.service;
 
+import book.com.library.model.Review;
+import book.com.library.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    public List<Review> getAllReviews(){
+        return reviewRepository.findAll();
+    }
 }
